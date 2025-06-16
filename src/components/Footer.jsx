004653409983df23a9e10a978ebc12b6f4e64658
ikhsan13-05 <ikhsanmuhammad0013@gmail.com>
2025-06-16 20:39:46 +0800

    Tambah garis di footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
     return (
       <footer className="text-white py-8 text-center text-xl">
         <div className="max-w-3xl mx-auto px-4 text-shadow-lg/50">
+        <div className="h-px bg-white mb-3" />
           <p className="mb-2">&copy; 2025 Ulfa & Ihsan Wedding Invitation</p>
           <p className="text-white">
             Desain by UI 💕
